26d7f4cad28bb99088347081e36db4122962b60b
kic <107183332+kichian@users.noreply.github.com>
2023-07-04 04:36:43 +0100

    Update addexamp1.s

--- Chapter 02/addexamp1.s
+++ Chapter 02/addexamp1.s
@@ -6,7 +6,6 @@
 // Multiply 2 by -1 by using MOVN and then adding 1
 _start:	MOVN	W0, #2
 	ADD	W0, W0, #1
-
 // Setup the parameters to exit the program
 // and then call the kernel to do it.
 // W0 is the return code and will be what we
