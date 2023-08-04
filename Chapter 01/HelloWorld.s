235441f80f71443fd898efe70d113966a15606a5
vymoo <107595054+vymoo@users.noreply.github.com>
2023-08-04 07:07:40 +0100

    Update HelloWorld.s

--- Chapter 01/HelloWorld.s
+++ Chapter 01/HelloWorld.s
@@ -7,7 +7,7 @@
 //
 .global _start			// Provide program starting address to linker
 .align 2			// Make sure everything is aligned properly
-
+#
 // Setup the parameters to print hello world
 // and then call the Kernel to do it.
 _start: mov	X0, #1		// 1 = StdOut
